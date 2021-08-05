39c919fbb32811a3f40fa72a1e410066f4e13e05
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-08-05 11:14:46 -0400

    updating mutations & signup logic

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,15 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
@@ -14,12 +23,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+       
           <Navbar/>
           <Switch>
             <Route exact path='/' component={SearchBooks} />
             <Route exact path='/saved' component={SavedBooks} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
+        
       </Router>
     </ApolloProvider>
   );
