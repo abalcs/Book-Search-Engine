08e3160b0c85a0ced3571ce43da477f29263a39c
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-08-05 14:10:34 -0400

    updated apollo imports and front end page logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+
+import Navbar from './components/Navbar';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
   request: operation => {
@@ -17,7 +18,7 @@ const client = new ApolloClient({
     })
   },
   uri: '/graphql',
-  cache: new InMemoryCache(),
+  // cache: new InMemoryCache(),
 });
 
 function App() {
